2a95c43ca490fd66318a4be0e97569824e7af58d
microcai <microcaicai@gmail.com>
2014-11-14 14:52:11 +0800

    实现测试邮件发送, 注意, 测试而已, 实际不用这套哦!

--- src/register_moudle.cpp
+++ src/register_moudle.cpp
@@ -2,6 +2,92 @@
 #include "register_moudle.hpp"
 #include "user.pb.h"
 
+#include <boost/regex.hpp>
+#include <boost/asio/spawn.hpp>
+#include <openssl/x509.h>
+#include <openssl/pem.h>
+#include <openssl/x509v3.h>
+
+#include "internet_mail_format.hpp"
+
+template<class Handler>
+static void async_send_email_coro(boost::asio::io_service& io,std::string subject, std::string content, std::pair<std::string, std::string> attachment, Handler handler, boost::asio::yield_context yield_context)
+{
+	boost::system::error_code ec;
+	try{
+		int bytes_transfered;
+		boost::asio::streambuf readbuf;
+		std::string line;
+		// 好吧, 我写死了只给 peter 发邮件! 直接连 outlook.com 的发件地址!
+		boost::asio::ip::tcp::socket socket(io);
+
+		auto endpoint = boost::asio::ip::tcp::endpoint(boost::asio::ip::address::from_string("65.55.92.184"), 25);
+
+		boost::asio::async_connect(socket, & endpoint , yield_context);
+
+		bytes_transfered = boost::asio::async_read_until(socket, readbuf, boost::regex( "[0-9]*? .*?\n" ), yield_context);
+		readbuf.consume(bytes_transfered);
+
+		line = "HELO avplayer.org\r\n";
+		boost::asio::async_write(socket, boost::asio::buffer(line), yield_context);
+
+		bytes_transfered = boost::asio::async_read_until(socket, readbuf, boost::regex( "[0-9]*? .*?\n" ), yield_context);
+		readbuf.consume(bytes_transfered);
+		// 发送 mail from
+		line = "MAIL FROM: avrouter@avplayer.org\r\n";
+		boost::asio::async_write(socket, boost::asio::buffer(line), yield_context);
+		bytes_transfered = boost::asio::async_read_until(socket, readbuf, boost::regex( "[0-9]*? .*?\n" ), yield_context);
+		readbuf.consume(bytes_transfered);
+		// 发送 rcpt to
+		line = "rcpt to: peter_future@outlook.com\r\n";
+		boost::asio::async_write(socket, boost::asio::buffer(line), yield_context);
+		bytes_transfered = boost::asio::async_read_until(socket, readbuf, boost::regex( "[0-9]*? .*?\n" ), yield_context);
+		readbuf.consume(bytes_transfered);
+
+		// 发送邮件主体
+		line = "DATA\r\n";
+		boost::asio::async_write(socket, boost::asio::buffer(line), yield_context);
+		bytes_transfered = boost::asio::async_read_until(socket, readbuf, boost::regex( "[0-9]*? .*?\n" ), yield_context);
+		readbuf.consume(bytes_transfered);
+
+		InternetMailFormat imf;
+		imf.header["from"] = "avrouter@avplayer.org";
+		imf.header["to"] = "peter_future@outlook.com";
+		imf.header["subject"] = subject;
+		imf.header["content-type"] = "text/plain; charset=utf8";
+		// 囧, 不知道怎么发附件了, 算了, 反正 是 PEM 格式文本, 直接带这里了
+		line = "Hello, peter , this is from avrouter, please sign the CSR bellow\r\n\r\n\r\n";
+		line += attachment.second;
+		imf.body = line ;
+
+		std::ostream sendbuf(&readbuf);
+		imf_write_stream(imf, sendbuf);
+
+		bytes_transfered = boost::asio::async_write(socket, readbuf.data(), yield_context);
+		readbuf.commit(bytes_transfered);
+		bytes_transfered = boost::asio::async_read_until(socket, readbuf, boost::regex( "[0-9]*? .*?\n" ), yield_context);
+		readbuf.consume(bytes_transfered);
+
+		line = "\r\n.\r\n";
+		boost::asio::async_write(socket, boost::asio::buffer(line), yield_context);
+		bytes_transfered = boost::asio::async_read_until(socket, readbuf, boost::regex( "[0-9]*? .*?\n" ), yield_context);
+		readbuf.consume(bytes_transfered);
+	}catch(const boost::system::error_code& ec)
+	{
+		io.post(std::bind(handler,ec));
+	}
+
+	io.post(std::bind(handler,ec));
+}
+
+
+// 暂时的嘛, 等 CA 签名服务器写好了, 这个就可以删了.
+template<class Handler>
+static void async_send_email(boost::asio::io_service& io, std::string subject, std::string content, std::pair<std::string, std::string> attachment, Handler handler)
+{
+	boost::asio::spawn(io, boost::bind(async_send_email_coro<Handler>, boost::ref(io), subject, content, attachment, handler, _1));
+}
+
 namespace av_router {
 
 	register_moudle::register_moudle(io_service_pool& io_pool, database& db)
@@ -60,10 +146,12 @@ namespace av_router {
 
 		}
 
+		std::string user_name = register_msg->user_name();
+
 		// 确定是合法的 CSR 证书, 接着数据库内插
-		m_database.register_user(register_msg->user_name(),register_msg->rsa_pubkey(),
+		m_database.register_user(user_name,register_msg->rsa_pubkey(),
 			register_msg->mail_address(), register_msg->cell_phone(),
-			[](bool result)
+			[&, this](bool result)
 			{
 				// 插入成功了, 那就立马签名出证书来
 				if(result)
@@ -77,6 +165,27 @@ namespace av_router {
 					// TODO 返回注册成功信息 将证书一并返回
 
 					// FIXME 如果签名失败, 则回滚数据库, 签名很少会失败的吧.
+
+					// FIXME & NOTE
+					// 目前暂时的做法是给 peter 发一封邮件, 然后就告诉用户注册成功
+					// 当然这只是暂时的做法, 目的是保证现在测试阶段能跑过这个注册流程罢了
+					// 客户端可以开始轮询来获取证书, 也可以等收到邮件后再登录, 那个时候保证能拿到自己的证书
+					std::shared_ptr<BIO> bio(BIO_new(BIO_s_mem()), BIO_free);
+
+					PEM_write_bio_X509_REQ(bio.get(), csr.get());
+
+					unsigned char * PEM_CSR = NULL;
+					auto PEM_CSR_LEN = BIO_get_mem_data(bio.get(),&PEM_CSR);
+
+					// 开始发邮件
+					async_send_email(m_io_service_pool.get_io_service(),
+						"[CSR] Automanted CSR from AVROUTER test version",
+						"RT, 这是一封 avrouter 自动发送的邮件, 请表在意哈!",
+						std::make_pair<std::string, std::string>( user_name + ".csr", std::string((char*)PEM_CSR, PEM_CSR_LEN)),
+					[](boost::system::error_code ec)
+					{
+
+					});
 				}
 			}
 		);
